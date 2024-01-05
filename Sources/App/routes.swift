1a3ee2eed965ea4a048cfa7b677761e17405d056
Lucian Suta <lucisuta@gmail.com>
2024-01-05 18:45:16 +0200

    Added a hardcoded list of courses

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,7 +1,24 @@
 import Vapor
 
+public struct Course: Encodable {
+	var id: UUID
+	var code: String
+	var name: String
+}
+
+let courses = [
+	Course(id: UUID.init(), code: "CSE100", name: "Introduction to Programming"),
+	Course(id: UUID.init(), code: "CSE101", name: "Introduction to Algorithms"),
+	Course(id: UUID.init(), code: "CSE102", name: "Advanced Data Structures"),
+]
+
+struct MainPageContext: Encodable {
+	var title: String
+	var courses: [Course]
+}
+
 func routes(_ app: Application) throws {
 	app.get { req async throws in
-		try await req.view.render("index", ["title": "Trainee"])
+		try await req.view.render("index", MainPageContext(title: "Trainee", courses: courses))
 	}
 }
