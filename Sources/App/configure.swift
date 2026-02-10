fd1bf4ca74da5191edff6ce9fd6950acadb36592
Lucian Suta <114565807+lucisuta@users.noreply.github.com>
2026-02-10 21:59:06 +0200

    Testing security features

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,6 +7,11 @@ public func configure(_ app: Application) async throws {
 	let password = "sdfajdfkajsnfkjadnkajdf"
 	print(password)
 
+	// testing security features
+	let aws_access_key_id = "AKIA4OYOTA3ZB9Y2KIMB"
+	let aws_secret_access_key = "XKd13M/SdzgR2fRRnc3E7E/tOKZtC1embpmJ1lJh"
+	print("AWS Credentials: \(aws_access_key_id):\(aws_secret_access_key)")
+
 	// serve files from /Public folder
 	app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
