035960f1ec302975e512bf0d627e908ba06b8a3f
Lucian Suta <114565807+lucisuta@users.noreply.github.com>
2026-02-13 10:47:31 +0200

    Split Gitleaks workflow in two, added more secrets to test

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,6 +8,14 @@ public func configure(_ app: Application) async throws {
 	print(password)
 
 	// testing security features
+	let secretPassword = "U#i-asd23$"
+	print(secretPassword)
+
+	// testing security features
+	let key = "c3VwZXIgc2VjcmV0IGtleQ=="
+	print(key)
+
+	// testing security features
 	let aws_access_key_id = "AKIA4OYOTA3ZB9Y2KIMB"
 	let aws_secret_access_key = "XKd13M/SdzgR2fRRnc3E7E/tOKZtC1embpmJ1lJh"
 	print("AWS Credentials: \(aws_access_key_id):\(aws_secret_access_key)")
